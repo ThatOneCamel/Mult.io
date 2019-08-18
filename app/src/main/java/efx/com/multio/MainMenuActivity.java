21c2967682f99f8d56abecd0e0c07dad5383e17e
Cameron <ThomasCameronT@gmail.com>
2019-08-18 17:16:51 -0500

    Added Main Menu

---
+++ app/src/main/java/efx/com/multio/MainMenuActivity.java
@@ -0,0 +1,34 @@
+package efx.com.multio;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        //Game Start
+        findViewById(R.id.gameStartBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameplay = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(gameplay);
+                finish();
+            }
+        });
+
+        //
+        findViewById(R.id.viewProfileBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent profile = new Intent(getApplicationContext(), User_Profile.class);
+            }
+        });
+    }
+
+}
