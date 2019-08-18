679d9f0fb3b06dae594a35edeb4c4ee6d9c12d73
Tommy <trpipher@gmail.com>
2019-08-18 18:45:08 -0500

    Count Down
    +Count Down
    +Count Down Fragment

--- app/src/main/java/efx/com/multio/CampaignActivity.java
+++ app/src/main/java/efx/com/multio/CampaignActivity.java
@@ -1,9 +1,12 @@
 package efx.com.multio;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,9 +16,13 @@ public class CampaignActivity extends AppCompatActivity {
 
     EquationFragment mathFragment;
     InputButtons inputFragment;
+    CountDownScreen countDownFragment;
     ArrayList<GameHandler.Problem> problems;
     TextView editor;
     GameHandler Game;
+    ImageButton backButton;
+    CountDownTimer timer;
+    private long timeLeft = 3500;
 
     int answer;
 
@@ -24,30 +31,64 @@ public class CampaignActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign);
 
+
+
         mathFragment = (EquationFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_main);
-        inputFragment = (InputButtons) getSupportFragmentManager().findFragmentById(R.id.InputButtons);
+        inputFragment = (InputButtons) getSupportFragmentManager().findFragmentById(R.id.fragment_buttons);
+        countDownFragment = (CountDownScreen) getSupportFragmentManager().findFragmentById(R.id.fragment_countdown);
         editor = findViewById(R.id.numInput);
+        backButton = findViewById(R.id.backButton);
+
+        mathFragment.getView().setVisibility(View.INVISIBLE);
+        inputFragment.getView().setVisibility(View.INVISIBLE);
+        backButton.setVisibility(View.INVISIBLE);
 
         Game = new GameHandler();
         Game.generateProblems();
         mathFragment.load(Game.getProblem().numberA,Game.getProblem().numberB);
-
-
-        /*
-        mathFragment.placeholder.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+/*
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
-                    Log.i("KEY PRESSED","Enter pressed");
-                    checkAnswer(null);
-                }
-                return false;
+            public void run() {
+
             }
-        });
-        */
+        },1);
+*/
 
 
-//        Log.d("TAGGED", problems.toString());
+        timer = new CountDownTimer(timeLeft,100) {
+            @Override
+            public void onTick(long l) {
+                timeLeft = l;
+                updateStartCounter();
+            }
+
+            @Override
+            public void onFinish() {
+                loadGame();
+            }
+        }.start();
+
+
+        updateStartCounter();
+
+
+    }
+
+    public void updateStartCounter()
+    {
+        String timeStr = "";
+        timeStr += (int) timeLeft/1000;
+
+        countDownFragment.updateCounter(timeStr);
+    }
+
+    public void loadGame()
+    {
+        mathFragment.getView().setVisibility(View.VISIBLE);
+        inputFragment.getView().setVisibility(View.VISIBLE);
+        backButton.setVisibility(View.VISIBLE);
+        countDownFragment.getView().setVisibility(View.INVISIBLE);
     }
 
     public void checkAnswer(View n){
---
+++ app/src/main/java/efx/com/multio/CountDownScreen.java
@@ -0,0 +1,42 @@
+package efx.com.multio;
+
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CountDownScreen extends Fragment {
+
+
+    TextView timeView;
+
+    public CountDownScreen() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View fragView = inflater.inflate(R.layout.fragment_count_down_screen, container, false);
+       timeView = fragView.findViewById(R.id.timeText);
+
+
+        return fragView;
+    }
+
+    public void updateCounter(String str){
+        timeView.setText(str);
+    }
+
+
+
+}
