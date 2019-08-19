f559a69a47879bebf6a71672cbca9b8b4a9b0765
Tommy <trpipher@gmail.com>
2019-08-19 11:25:47 -0500

    In Game User
    +New fragment to handle user info in-game

---
+++ app/src/main/java/efx/com/multio/EndgameScreenActivity.java
@@ -0,0 +1,15 @@
+package efx.com.multio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EndgameScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endgame_screen);
+
+
+    }
+}
