10ded47ccdd802141ef051690facccd876c4459b
Tommy <trpipher@gmail.com>
2019-08-19 12:38:54 -0500

    End Game Screen
    +New End Game Screen
    +Main Menu Screen button for end game testing

--- app/src/main/java/efx/com/multio/EndgameScreenActivity.java
+++ app/src/main/java/efx/com/multio/EndgameScreenActivity.java
@@ -1,15 +1,39 @@
 package efx.com.multio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class EndgameScreenActivity extends AppCompatActivity {
+
+    TextView scoreText;
+    String score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(getIntent().getExtras() != null)
+        {
+            score = getIntent().getStringExtra("Score");
+        }
         setContentView(R.layout.activity_endgame_screen);
+        scoreText = findViewById(R.id.scoreNum);
+        scoreText.setText(score);
 
+    }
+
+    public void Menu(View v) {
+        Intent endIntent = new Intent(this,MainMenuActivity.class);
+        startActivity(endIntent);
+        finish();
+    }
+
+    public void Replay(View v) {
+        Intent endIntent = new Intent(this,CampaignActivity.class);
+        startActivity(endIntent);
+        finish();
 
     }
 }
